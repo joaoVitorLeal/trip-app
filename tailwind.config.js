0abcee7c61b4ab94c58cd82a9f230fa74bd5abaf
joaoVitorLeal <joaoleal98@outlook.com>
2025-08-18 17:58:49 -0300

    chore/config: Add Tailwind dependencies and configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Observará arquivos html e ts dentro de pastas ou subpastas em 'src/' para aplicar estilização.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
