128fac80fdf14479e4846b5f55431345fcc74e4b
joaoVitorLeal <joaoleal98@outlook.com>
2025-08-20 18:25:30 -0300

    feat: add authentication guard for children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { authGuard } from './auth.guard';
 
 // 
 const routes: Routes = [
   {
+    path: '',
+    component: LandingpageComponent,
+    pathMatch: 'full'
+  },
+  {
     path: 'pages',
     loadChildren: () => import('./template/template.module').then(m => m.TemplateModule), // Impotando todas as rotas do TemplateModule, carregando-a dinamicamente de forma lazy load (somente quando requisitado)
-    data: { title: 'Trip App', subtitle: 'Explore, dream, and create memories everywhere' }
+    canActivate: [ authGuard ] // Permite acesso apenas a usuários autenticados
   }
 ];
 
