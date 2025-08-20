757be65983b8fa372febd8bd68e96b5da48dcef2
joaoVitorLeal <joaoleal98@outlook.com>
2025-08-20 18:18:06 -0300

    chore(env):  Add application environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    apiBaseUrl: 'http://localhost:3000',
+    oauth: {
+        googleClientId: '209845474736-rhettgmgp2ii8anu78qhrlp7f4jee4hv.apps.googleusercontent.com'
+    }
+};
