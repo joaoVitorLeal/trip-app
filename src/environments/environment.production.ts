7f7c1f67983c0d4d1a0be5a34ed47ba5cb6f8c1c
joaoVitorLeal <joaoleal98@outlook.com>
2025-08-20 18:19:45 -0300

    chore(env):  configure production and development environment files

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'http://localhost:4000',
+    oauth: {
+        googleClientId: '209845474736-rhettgmgp2ii8anu78qhrlp7f4jee4hv.apps.googleusercontent.com'
+    }
+};
